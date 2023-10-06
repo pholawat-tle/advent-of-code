807e0ccd43206ea70b64a07530942a99795cd92e
Pholawat Tangsatit <pholawat.tangsatit@hotmail.com>
2023-10-06 18:35:56 +0700

    refactor - each year is a folder of its own

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 use std::fs;
 
 pub fn read_txt(file_name: &str) -> String {
+    let year_from_file_name = file_name
+        .split('_')
+        .nth(1)
+        .expect("Error parsing year from file name");
+
+    let actual_file_path = format!("input/{}/{}.txt", year_from_file_name, file_name);
+
     let error_message = format!("Error reading file: {}", file_name);
-    let file_path = format!("input/{}.txt", file_name);
-    fs::read_to_string(file_path).expect(&error_message)
+    fs::read_to_string(actual_file_path).expect(&error_message)
 }
